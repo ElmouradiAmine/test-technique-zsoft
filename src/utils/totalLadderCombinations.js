12bfc3913a3aaf01ff3c701119ea0be2ca23f598
Amine Elmouradi <elmouradi.amine98@gmail.com>
2021-02-01 17:57:59 +0100

    use iterative approach

--- src/utils/totalLadderCombinations.js
+++ src/utils/totalLadderCombinations.js
@@ -1,10 +1,26 @@
-const memo = {};
+// const memo = {};
+
+// const totalLadderCombinations = (n) => {
+//   if (memo[n]) return memo[n];
+//   if (n <= 1) return 1;
+//   memo[n] = totalLadderCombinations(n - 1) + totalLadderCombinations(n - 2);
+//   return memo[n];
+// };
 
 const totalLadderCombinations = (n) => {
-  if (memo[n]) return memo[n];
-  if (n <= 1) return 1;
-  memo[n] = totalLadderCombinations(n - 1, memo) + totalLadderCombinations(n - 2, memo);
-  return memo[n];
+  let number = n;
+  let a = 1;
+  let b = 0;
+  let temp;
+
+  while (number >= 0) {
+    temp = a;
+    a += b;
+    b = temp;
+    number -= 1;
+  }
+
+  return b;
 };
 
 module.exports = totalLadderCombinations;
